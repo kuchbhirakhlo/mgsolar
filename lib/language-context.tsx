1d5df5efc0d77cd10406ea27d33d0081dc4a00ef
Aviral Shukla <avi.sr00@gmail.com>
2026-04-07 16:09:17 +0530

    newupdatemgsolar

--- lib/language-context.tsx
+++ lib/language-context.tsx
@@ -9,7 +9,7 @@ interface LanguageContextType {
   t: (typeof translations)[Language];
 }
 
-const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
+export const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
 export function LanguageProvider({ children }: { children: React.ReactNode }) {
   const [language, setLanguage] = useState<Language>('en');
